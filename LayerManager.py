d7823f447ba317591185661f37c017d9c3fa1426
YZhou0413 <zhouyn0413@163.com>
2024-05-16 17:06:45 +0200

    dragdrop Event need to be fixed

--- LayerManager.py
+++ LayerManager.py
@@ -1,81 +1,114 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QListWidget, QListWidgetItem, QAbstractItemView
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import pyqtSignal, QEvent
 from PyQt6.QtGui import QCursor
-from collections import OrderedDict
+from collections import OrderedDict, deque
 from PreviewWidget import PreviewGraphicsView
 class MyListWidget(QListWidget):
-    itemMoved = pyqtSignal(int, int)  # 自定义信号
+    itemMoved = pyqtSignal(int, int)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setAcceptDrops(True)
-    def dropEvent(self, event):
+        self.setDragDropMode(QAbstractItemView.DragDropMode.InternalMove)  
+
+    def dropEvent(self, event: QEvent):
         super().dropEvent(event)
         from_index = self.currentRow()
-        to_index = self.row(self.itemAt(self.viewport().mapFromGlobal(QCursor.pos()))) if self.itemAt(self.viewport().mapFromGlobal(QCursor.pos())) else -1
+        to_index = self.indexAt(event.position().toPoint()).row() if self.indexAt(event.position().toPoint()).isValid() else -1
         self.itemMoved.emit(from_index, to_index)
+
+
 
 class LayerManager(QWidget):
     layers_change = pyqtSignal(dict)
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Layer Manager')
         self.layers = OrderedDict()
         self.selected_layer_index = -1
+        self.preview = PreviewGraphicsView()
         self.init_ui()
 
     def init_ui(self):
-        self.layer_list = MyListWidget()  
-        self.layer_list.setDragDropMode(QAbstractItemView.DragDropMode.InternalMove)  
+        self.layer_list = MyListWidget()    
         self.layer_list.currentRowChanged.connect(self.set_layer_index)
-        self.layer_list.itemMoved.connect(self.move_layer)  # 绑定自定义信号
+        self.layer_list.itemMoved.connect(self.handle_item_moved)  
 
-        self.Lower_button = QPushButton("Lower Layer")
-        self.Raise_button = QPushButton("Raise Layer")
+        self.lower_button = QPushButton("Lower Layer")
+        self.raise_button = QPushButton("Raise Layer")
         
-        self.Lower_button.clicked.connect(self.raise_layer)
-        self.Raise_button.clicked.connect(self.lower_layer)
+        self.lower_button.clicked.connect(lambda: self.lower_layer(self.selected_layer_index))
+        self.raise_button.clicked.connect(lambda: self.raise_layer(self.selected_layer_index))
         
         layout = QVBoxLayout()
         layout.addWidget(self.layer_list)
-        layout.addWidget(self.Lower_button)
-        layout.addWidget(self.Raise_button)
+        layout.addWidget(self.lower_button)
+        layout.addWidget(self.raise_button)
         self.setLayout(layout)
-        self.preview = PreviewGraphicsView()
 
     def set_selected_styles(self, layers):
         self.layers = layers
         self.refresh_layers()
-        self.update_layers()  # 添加这一行来确保在更新 self.layers 后立即更新 Preview
+        self.update_layers() 
 
-    def lower_layer(self):
-        if self.selected_layer_index > 0:
-            self.move_layer(self.selected_layer_index, self.selected_layer_index - 1)
+    def move_item_in_list(self, from_index, to_index):
+        if from_index == to_index or from_index < 0 or to_index < 0 or from_index >= self.layer_list.count() or to_index >= self.layer_list.count():
+            return
 
-    def raise_layer(self):
-        if self.selected_layer_index < len(self.layers) - 1:
-            self.move_layer(self.selected_layer_index, self.selected_layer_index + 1)
+        # 在列表中移动项
+        item = self.layer_list.takeItem(from_index)
+        self.layer_list.insertItem(to_index, item)
+        self.layer_list.setCurrentRow(to_index)
 
+        # 触发字典移动函数
+        self.move_item_in_dict(from_index, to_index)
+        self.refresh_layers()
+        self.update_layers()
 
-    def move_layer(self, from_index, to_index):
-    # 获取被移动的键值对
+    # 字典移动函数
+    def move_item_in_dict(self, from_index, to_index):
         keys = list(self.layers.keys())
         values = list(self.layers.values())
+        if to_index < 0 or to_index >= len(keys):
+            return
+        # 在字典中进行索引转换
+        dict_from_index = len(keys) - 1 - from_index
+        dict_to_index = len(keys) - 1 - to_index
 
-        # 交换位置
-        keys[from_index], keys[to_index] = keys[to_index], keys[from_index]
-        values[from_index], values[to_index] = values[to_index], values[from_index]
+        # 交换键和值
+        keys[dict_from_index], keys[dict_to_index] = keys[dict_to_index], keys[dict_from_index]
+        values[dict_from_index], values[dict_to_index] = values[dict_to_index], values[dict_from_index]
 
-        # 重建字典
+        # 更新字典
         self.layers = OrderedDict(zip(keys, values))
 
+        # 刷新图层预览
         self.refresh_layers()
-        self.layer_list.setCurrentRow(to_index)
         self.update_layers()
+
+    # Lower按钮点击事件处理函数
+    def lower_layer(self, selected_layer_index):
+        to_index = selected_layer_index + 1  # 更新选中图层的索引
+        self.move_item_in_dict(selected_layer_index, to_index)
+        self.layer_list.setCurrentRow(to_index)
+
+
+    # Raise按钮点击事件处理函数
+    def raise_layer(self, selected_layer_index):
+        to_index = selected_layer_index - 1  # 更新选中图层的索引
+        self.move_item_in_dict(selected_layer_index, to_index)
+        self.layer_list.setCurrentRow(to_index)
+
+
+    # 列表项移动事件处理函数
+    def handle_item_moved(self, from_index, to_index):
+        self.move_item_in_list(from_index, to_index)
 
     def refresh_layers(self):
         self.layer_list.clear()
-        for layer_name in self.layers.keys():
+        for layer_name in reversed(list(self.layers.keys())):  # 从右到左遍历字典
             self.layer_list.addItem(layer_name)
 
     def set_layer_index(self, index):
